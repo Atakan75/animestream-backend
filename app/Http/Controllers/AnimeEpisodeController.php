ff2c5d41bae0b0b76ecab916192995e80b307653
Atakan <atakanseventyfive@gmail.com>
2025-02-11 20:07:01 +0300

    Remove caching from AnimeEpisodeController show method and wrap response in AnimeEpisodeResource

--- app/Http/Controllers/AnimeEpisodeController.php
+++ app/Http/Controllers/AnimeEpisodeController.php
@@ -1,42 +1,37 @@
 <?php
-
 namespace App\Http\Controllers;
 
 //use Illuminate\Http\Request;
 
-use App\Models\AnimeEpisode;
-
 use App\Http\Resources\AnimeEpisodeResource;
-use App\Models\UserWatchHistory;
+use App\Models\AnimeEpisode;
 
 class AnimeEpisodeController extends Controller
 {
     public function show($anime_slug, $episode)
     {
-        $animeEpisode = cache()->remember("anime_episode_{$episode}", 60 * 60 * 24, function () use ($episode) {
-            return AnimeEpisode::with([
-                'season' => function ($query) {
-                    $query->with([
-                        'episodes' => function ($query) {
-                            $query->select('id', 'slug', 'name', 'season_id')
-                                ->orderBy('created_at', 'desc');
-                        },
-                    ]);
-                },
-                'comments' => function ($query) {
-                    $query->with([
-                        'parent.parent.parent.parent.parent',
-                        'user' => function ($query) {
-                            $query->select('id', 'name');
-                        },
-                    ])->whereNull('parent_id')
-                        ->orderBy('created_at', 'desc');
-                },
-            ])->where('slug', $episode)->first();
-        });
+        $animeEpisode = AnimeEpisode::with([
+            'season'   => function ($query) {
+                $query->with([
+                    'episodes' => function ($query) {
+                        $query->select('id', 'slug', 'name', 'season_id')
+                            ->orderBy('created_at', 'desc');
+                    },
+                ]);
+            },
+            'comments' => function ($query) {
+                $query->with([
+                    'parent.parent.parent.parent.parent',
+                    'user' => function ($query) {
+                        $query->select('id', 'name');
+                    },
+                ])->whereNull('parent_id')
+                    ->orderBy('created_at', 'desc');
+            },
+        ])->where('slug', $episode)->first();
 
         return response_success([
-            'episode' => ($animeEpisode),
+            'episode' => new AnimeEpisodeResource($animeEpisode),
         ]);
     }
 }
