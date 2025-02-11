00d1f398e1c403b66534203220ca61ab1f602936
Atakan <atakanseventyfive@gmail.com>
2025-02-11 20:14:03 +0300

    Improve CommentResource with more robust null handling and conditional loading

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -15,11 +15,13 @@ class CommentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id ? $this->id : null,
-            'comment' => $this->comment ? $this->comment : null,
-            'user' => new UserResource($this->whenLoaded('user')),
-            'parent' => new CommentResource($this->whenLoaded('parent')),
-            'created_at' => $this->created_at ? $this->created_at : null,
+            'id' => $this->resource->id ?? null,
+            'comment' => $this->resource->comment ?? null,
+            'user' => $this->when($this->resource->user, new UserResource($this->resource->user)),
+            'parent' => $this->when($this->resource->parent, function() {
+                return $this->resource->parent ? new CommentResource($this->resource->parent) : null;
+            }),
+            'created_at' => $this->resource->created_at ?? null,
         ];
     }
 }
