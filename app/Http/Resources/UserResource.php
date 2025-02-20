8830bf522e2dcbfcbb2a709904de9df4c7c24310
Atakan <46746945+Atakan75@users.noreply.github.com>
2025-02-21 00:09:44 +0300

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'name' => $this->name ? $this->name : null,
             'email' => $this->email ? $this->email : null,
             'avatar' => $this->avatar ? config('app.url') . '/storage/user_avatars/' . md5($this->id) . '/' . $this->avatar->name : null,
+            'roles' => $this->whenLoaded('roles'),
             'created_at' => $this->created_at ? $this->created_at : null,
             'updated_at' => $this->updated_at ? $this->updated_at : null,
         ];
