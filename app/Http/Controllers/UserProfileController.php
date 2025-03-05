ee142c9afebd81383e033df7182d0bd005641693
Atakan <atakanseventyfive@gmail.com>
2025-03-05 16:01:42 +0300

    Refactor error handling in UserProfileController to use response_error and response_success

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -14,10 +14,10 @@ class UserProfileController extends Controller
         $user = User::where('name', $username)->with('avatar')->first();
 
         if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+            return response_error(['message' => 'User not found'], 404);
         }
 
-        return response()->json([
+        return response_success([
             'user' => new UserResource($user->load('roles')),
         ]);
     }
